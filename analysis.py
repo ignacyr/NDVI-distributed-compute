d29050e10453e5beeb68471c014af072e67c6c2e
ignacyr <ignacy.rogatty@gmail.com>
2022-12-11 03:03:55 +0100

    plot of acceleration comparison for different number of containers (cores)

--- analysis.py
+++ analysis.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("master/measurements.csv")
 df = df.loc[df["ram_limit"] == 1.5]
+df = df.groupby("containers", as_index=False).mean()
 df = df.sort_values(by="containers")
-df["acceleration"] = 220 / df["total_time"]
+df["acceleration"] = df["total_time"][1] / df["total_time"]
 
 print(df)
 
